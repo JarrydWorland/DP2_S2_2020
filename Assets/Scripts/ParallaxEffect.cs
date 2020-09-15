f6c42afbd9054a971fbe65be3673675fbcbc458a
Parker Young <pmyoung@outlook.com.au>
2020-09-15 19:12:05 +1000

    Game map complete
    Game map including assets completed on this branch. Parallax added for visual appeal and dynamics.

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    private float length, initPos;
+    public GameObject cam;
+    public float parallaxEffectAmount;
+
+    void Start()
+    {
+        initPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffectAmount));
+        float dist = (cam.transform.position.x * parallaxEffectAmount);
+
+        transform.position = new Vector3(initPos + dist, transform.position.y, transform.position.z);
+
+        if (temp > initPos + length) initPos += length;
+        else if (temp < initPos - length) initPos -= length;
+    }
+}
