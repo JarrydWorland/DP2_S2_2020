b653bb9a0191bb308358ad68da8e7e6467fe8712
jarryd.worland <101096903@student.swin.edu.au>
2020-09-16 16:26:06 +1000

    - Player Character now a prefab - Player Character can now Move - Player Character limited to Camera Bounds

---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed;
+    public Camera MainCamera;
+    private Vector3 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += movement * speed;
+
+    }
+
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight);
+        transform.position = viewPos;
+    }
+}
