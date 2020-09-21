fa84d36ee5b10bc2f53d9f2ccf932c5facb42b5e
Sam Huffer <samhuffer01@gmail.com>
2020-09-22 09:57:01 +1000

    Outline of enemy spawning methods, and struct for holding spawn data.

--- Assets/Scripts/Enemies/EnemySpawnManager.cs
+++ Assets/Scripts/Enemies/EnemySpawnManager.cs
@@ -2,17 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawnManager : MonoBehaviour
+/// <summary>
+/// Manages the spawning of Enemies on request.
+/// </summary>
+public class EnemySpawnManager : SerializableSingleton<EnemySpawnManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    //Private Fields---------------------------------------------------------------------------------------------------------------------------------
+
+    //Serialized Fields----------------------------------------------------------------------------
+
+    [SerializeField] private TextAsset waveDataFile;
+
+    //Non-Serialized Fields------------------------------------------------------------------------
+
+    private Dictionary<string, List<SpawnData>> waveData;
+
+    //Public Properties------------------------------------------------------------------------------------------------------------------------------
+
+    //Basic Public Properties----------------------------------------------------------------------                                                                                                                          
+    
+    /// <summary>
+    /// Has the data detailing the waves of enemies been loaded successfully?
+    /// </summary>
+    public bool IsWaveDataLoaded { get => false; }
+
+    //Initialization Methods-------------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Awake() is run when the script instance is being loaded, regardless of whether or not the script is enabled. 
+    /// Awake() runs before Start().
+    /// </summary>
+    protected override void Awake()
     {
-        
+        base.Awake();
+        LoadWaveDataFromFile();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Loads the data on waves of enemies from the text file and saves them for later.
+    /// </summary>
+    private void LoadWaveDataFromFile()
     {
+        //if text file is null
+
+            //Error message
+
+        //else
+
+            //Load text file
+
+            //for each line
+
+                //if line is comment
+
+                    //ignore
+
+                //else if line is invalid
+
+                    //Error message
+
+                //else
+                
+                    //Translate line to spawn data
+
+                    //Add spawn data to list
+    }
+
+    //Triggered Methods------------------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Spawns a wave of enemies.
+    /// </summary>
+    /// <param name="id">The ID of the wave to be spawned.</param>
+    public void SpawnWave(string id)
+    {
+        //if dictionary has non-empty list matching id
         
+            //for each spawn data
+
+                //Instantiate enemy according to spawn data via EnemyFactory
+
+        //else
+
+            //Error message
     }
 }
