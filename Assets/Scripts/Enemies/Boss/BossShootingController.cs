6866e0d7282e3289bb6afee92701599daa29447a
Jordan Epstein <102747419@student.swin.edu.au>
2020-10-06 18:11:22 +1100

    Implemented basic BossShootingController.as

--- Assets/Scripts/Enemies/Boss/BossShootingController.cs
+++ Assets/Scripts/Enemies/Boss/BossShootingController.cs
@@ -1,18 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Handles whether or not bosses shoot at the player.
+/// </summary>
 public class BossShootingController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Non-Serialized Fields------------------------------------------------------------------------
+
+    private Weapon weapon;
+
+    //Initialization Methods-------------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Awake() is run when the script instance is being loaded, regardless of whether or not the script is enabled. 
+    /// Awake() runs before Start().
+    /// </summary>
+    private void Awake()
     {
-        
+        weapon = GetComponentInChildren<Weapon>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //Core Recurring Methods-------------------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Update() is run every frame.
+    /// </summary>
+    private void Update()
     {
-        
+        Shoot();
+    }
+
+    //Recurring Methods (Update())--------------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// Shoots the weapon if it is able to.
+    /// </summary>
+    private void Shoot()
+    {
+        if (weapon.CanShoot() && WantToShoot())
+        {
+            weapon.Shoot();
+        }
+    }
+
+    /// <summary>
+    /// Checks if the boss wants to shoot at the player.
+    /// </summary>
+    /// <returns>Whether the boss wants to shoot at the player or not.</returns>
+    private bool WantToShoot()
+    {
+        return true;
     }
 }
